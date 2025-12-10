31dffb424c4596492cda0647fba3488840098e41
Bolu Akintoye <75408797+Boluwahduro@users.noreply.github.com>
2025-12-10 13:39:25 -0500

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,11 @@
+CREATE TABLE IF NOT EXISTS trips_raw (
+    VendorID INTEGER,
+    tpep_pickup_datetime TEXT,
+    tpep_dropoff_datetime TEXT,
+    passenger_count INTEGER,
+    trip_distance REAL,
+    fare_amount REAL,
+    total_amount REAL
+);
 
+  
